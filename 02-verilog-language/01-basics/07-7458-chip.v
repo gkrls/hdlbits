5a6f6ca1a0acd086f0998eb0a0aa8cf1bbbcb8f5
gkrls <gkrls.codes@gmail.com>
2024-12-01 02:14:07 +0100

    fix formatting

--- 02-verilog-language/01-basics/07-7458-chip.v
+++ 02-verilog-language/01-basics/07-7458-chip.v
@@ -1,8 +1,7 @@
-module top_module ( 
-    input p1a, p1b, p1c, p1d, p1e, p1f,
-    output p1y,
-    input p2a, p2b, p2c, p2d,
-    output p2y );
+module top_module ( input p1a, p1b, p1c, p1d, p1e, p1f,
+                    output p1y,
+                    input p2a, p2b, p2c, p2d,
+                    output p2y );
 
   wire p1y_a;
   wire p1y_b;
